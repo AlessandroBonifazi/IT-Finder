b342c1c62ac23d55ff21f4a64c11b8d8d1c84cae
gioiascari <gioia.ascari96@gmail.com>
2022-06-23 15:40:38 +0200

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
             font-family: 'Nunito', sans-serif;
             font-weight: 200;
             height: 100vh;
-            margin: 0;
+            /* margin: 0; */
         }
 
         .full-height {
