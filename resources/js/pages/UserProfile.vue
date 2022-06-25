ec1033528adba8b0e47bf48d606fda9a9c664a11
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-25 17:41:20 +0200

    correct input link

--- resources/js/pages/UserProfile.vue
+++ resources/js/pages/UserProfile.vue
@@ -1,17 +1,15 @@
 <template>
     <div>
-        <UserCardComponent/>
+        <UserCardComponent />
     </div>
 </template>
 <script>
-import UserCardComponent from '../components/UserCardComponent.vue'
+import UserCardComponent from "./../components/UserCardComponent.vue";
 export default {
-    name: 'UserProfile',
-    components:{
-        UserCardComponent
-    }
-}
+    name: "UserProfile",
+    components: {
+        UserCardComponent,
+    },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
