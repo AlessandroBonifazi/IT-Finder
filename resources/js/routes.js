069d9188e40927c5c9801355a1fcd5c9f5f5869e
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-25 17:39:08 +0200

    upd routes & add a convention rule

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,36 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+// ! IMPORTANT: for our convention we import pages like this.
+// ! Its safer and avoid many bugs
+import MainPage from "./pages/MainPageComponent.vue";
+import UserProfile from "./pages/UserProfile.vue";
+
 const router = new VueRouter({
-  mode: 'history', 
-  routes: [
-  {
-    path: '/',
-    name: 'guest-home',
-    component: () => import('../js/pages/MainPageComponent.vue')
-  },
-  {
-    path: '/user-profile',
-    name: 'user-profile',
-    component: () => import('../js/pages/UserProfile.vue')
-  },
-  //  {
-  //    path: '/signup',
-  //    name: 'signup',
-  //    component: () => import('../js/pages/SignUpComponent.vue')
-  //  },
-  
-]});
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   mode: 'history',
-//   linkExactActiveClass: 'active',
-//   linkActiveClass: 'active',
-//   routes
-// })
-
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "guest-home",
+            component: MainPage,
+        },
+        {
+            path: "/user-profile",
+            name: "user-profile",
+            component: UserProfile,
+        },
+    ],
+});
 
 export default router;
