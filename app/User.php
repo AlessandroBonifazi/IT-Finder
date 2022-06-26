242e2b18242321246270ddf9cc8d3fa3621a4abb
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-26 18:36:32 +0200

    upd fillable info for registration

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = ["name", "email", "password"];
+    protected $fillable = ["user_name", "email", "password"];
 
     /**
      * The attributes that should be hidden for arrays.
