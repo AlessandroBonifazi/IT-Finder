0f03046a6817325e233a1a2d1e2b00ba18d5271d
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-30 01:17:32 +0200

    created header component

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-12">
+                <div class="row justify-content-between">
+                    <!-- LOGO -->
+                    <div class="col-3">
+                        <h1>ITFinder</h1>
+                    </div>
+                    <!-- END LOGO -->
+                    <!-- USER CONTROLS -->
+                    <div
+                        class="col d-flex align-items-center justify-content-end"
+                    >
+                        <div class="row">
+                            <div class="col">
+                                <button
+                                    class="itf-btn itf-btn-tertiary itf-btn-small"
+                                    @click="searchRedirect"
+                                >
+                                    Search
+                                </button>
+                                <button
+                                    class="itf-btn itf-btn-tertiary itf-btn-small"
+                                    @click="register"
+                                >
+                                    Register
+                                </button>
+                                <button
+                                    class="itf-btn itf-btn-primary itf-btn-small"
+                                    @click="login"
+                                >
+                                    Login
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- END USER CONTROLS -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent",
+    data() {
+        return {
+            user: {},
+        };
+    },
+    methods: {
+        logout() {
+            window.axios.post("/logout").then(() => {
+                // this.$router.push("/");
+            });
+        },
+        login() {
+            this.$router.push("/login");
+        },
+        register() {
+            this.$router.push("/register");
+            // window.location.reload();
+        },
+        searchRedirect() {
+            this.$router.push("/search");
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../../sass/app";
+</style>
