f134802b5f0daf70a9288bd68da6cbd297171376
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-06-29 12:46:56 +0200

    layout fix

--- resources/js/pages/MainPageComponent.vue
+++ resources/js/pages/MainPageComponent.vue
@@ -1,21 +1,8 @@
 <template>
-    <!-- <div>
-        <h1>Main</h1>
-        <router-link to="/register">register</router-link>
-        <button
-            @click="
-                () => {
-                    logout();
-                }
-            "
-        >
-            Logout
-        </button>
-    </div> -->
     <main>
         <div class="container">
             <!-- banner -->
-            <div class="row">
+            <div class="row my-5">
                 <div class="col-6">
                     <h1>Your Next IT Specialist is Here</h1>
                     <p>Search and find best IT Specialist from all around the world for your one million project idea or business</p>
@@ -23,25 +10,28 @@
                 </div>
             </div>
             <!-- specialist role -->
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h2>What kind of specialist do you need?</h2>
+            <div class="container bg-green my-5">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>What kind of specialist do you need?</h2>
+                    </div>
                 </div>
-                <div class="row flex-wrap">
-                    <div class="col-3"><a href="#">Front-End</a></div>
-                    <div class="col-3"><a href="#">Back-End</a></div>
-                    <div class="col-3"><a href="#">Mobile</a></div>
-                    <div class="col-3"><a href="#">Analyst</a></div>
-                    <div class="col-3"><a href="#">Web Developer</a></div>
-                    <div class="col-3"><a href="#">Web Developer</a></div>
-                    <div class="col-3"><a href="#">Web Developer</a></div>
-                    <div class="col-3"><a href="#">Web Developer</a></div>
+
+                <div class="row flex-wrap justify-content-center">
+                    <div class="col-3 text-center my-2"><a href="#">Front-End</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Back-End</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Mobile</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Analyst</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
+                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
                 </div>
             </div>
             <!-- promo specialist -->
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 text-center">
                         <h2>Our best specialist</h2>
                     </div>
 
@@ -54,24 +44,24 @@
                         <div class="col-4"></div>
                     </div>
 
-                    <div class="col-12">
+                    <div class="col-12 d-flex justify-content-center">
                         <button class="btn">Discover More</button>
                     </div>
                 </div>
                 <!-- specialist banner -->
-                <div class="row">
+                <div class="row bg-green my-5">
                     <div class="col-5">
                         <h1>Find the talent needed to get your business growing.</h1>
                         <button class="btn">Get Started</button>
                     </div>
                 </div>
                 <!-- find next job section -->
-                <div class="row">
+                <div class="row my-5">
                     <div class="col-5">
                         <img src="" alt="">
                         <img src="" alt="">
                     </div>
-                    <div class="col-5 d-flex flex-column">
+                    <div class="col-5 offset-2 d-flex flex-column">
                         <div class="next-job">
                             <h3>Title</h3>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab magni sapiente neque recusandae reiciendis, perspiciatis magnam ducimus maxime voluptatibus natus!</p>
@@ -88,7 +78,7 @@
                 </div>
             </div>
             <!-- footer -->
-            <div class="container">
+            <div class="container my-5">
                 <div class="row">
                     footer
                 </div>
@@ -118,4 +108,8 @@ export default {
 
 <style lang="scss" scoped>
 @import "./../../sass/app";
+
+.bg-green{
+    background-color: $green-60;
+}
 </style>
