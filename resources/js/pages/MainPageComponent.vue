19d2a34820a95c607c28949f9f464641ecbb7a43
Robin9800 <96703277+Robin9800@users.noreply.github.com>
2022-06-29 16:28:04 +0200

    kind of specialist section content

--- resources/js/pages/MainPageComponent.vue
+++ resources/js/pages/MainPageComponent.vue
@@ -1,90 +1,136 @@
 <template>
-    <main>
-        <div class="container">
-            <!-- banner -->
-            <div class="row my-5">
-                <div class="col-6">
-                    <h1>Your Next IT Specialist is Here</h1>
-                    <p>Search and find best IT Specialist from all around the world for your one million project idea or business</p>
-                    <button class="btn">Discover More</button>
-                </div>
+    <div class="m-0 p-0">
+        <!-- banner -->
+        <div class="row my-5">
+            <div class="col-6">
+                <h1 class="krona">Your Next IT Specialist is Here</h1>
+                <p>
+                    Search and find best IT Specialist from all around the world
+                    for your one million project idea or business
+                </p>
+                <button class="btn">Discover More</button>
             </div>
-            <!-- specialist role -->
-            <div class="container bg-green my-5">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <h2>What kind of specialist do you need?</h2>
-                    </div>
-                </div>
+        </div>
+        <!-- specialist role -->
+        <svg width="1519" height="240" viewBox="0 0 1519 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" fill="#14453D"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" fill="#123C35"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" stroke="black"/>
+        </svg>
 
-                <div class="row flex-wrap justify-content-center">
-                    <div class="col-3 text-center my-2"><a href="#">Front-End</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Back-End</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Mobile</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Analyst</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
-                    <div class="col-3 text-center my-2"><a href="#">Web Developer</a></div>
-                </div>
-            </div>
-            <!-- promo specialist -->
+        <div class="container-fluid bg-green-55">
             <div class="container">
                 <div class="row">
-                    <div class="col-12 text-center">
-                        <h2>Our best specialist</h2>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-4"></div>
-                        <div class="col-4"></div>
-                        <div class="col-4"></div>
-                        <div class="col-4"></div>
-                        <div class="col-4"></div>
-                        <div class="col-4"></div>
-                    </div>
-
-                    <div class="col-12 d-flex justify-content-center">
-                        <button class="btn">Discover More</button>
-                    </div>
-                </div>
-                <!-- specialist banner -->
-                <div class="row bg-green my-5">
-                    <div class="col-5">
-                        <h1>Find the talent needed to get your business growing.</h1>
-                        <button class="btn">Get Started</button>
-                    </div>
-                </div>
-                <!-- find next job section -->
-                <div class="row my-5">
-                    <div class="col-5">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                    </div>
-                    <div class="col-5 offset-2 d-flex flex-column">
-                        <div class="next-job">
-                            <h3>Title</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab magni sapiente neque recusandae reiciendis, perspiciatis magnam ducimus maxime voluptatibus natus!</p>
-                        </div>
-                        <div class="next-job">
-                            <h3>Title</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab magni sapiente neque recusandae reiciendis, perspiciatis magnam ducimus maxime voluptatibus natus!</p>
-                        </div>
-                        <div class="next-job">
-                            <h3>Title</h3>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab magni sapiente neque recusandae reiciendis, perspiciatis magnam ducimus maxime voluptatibus natus!</p>
-                        </div>
+                    <div class="col-12 text-center mb-4">
+                        <h2 class="yellow-60 krona">
+                            What kind of specialist do you need?
+                        </h2>
                     </div>
                 </div>
             </div>
-            <!-- footer -->
-            <div class="container my-5">
-                <div class="row">
-                    footer
+            <div class="row flex-wrap justify-content-center">
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Front-End</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Back-End</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Mobile</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Analyst</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Web Developer</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Web Developer</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Web Developer</a>
+                </div>
+                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                    <a href="#" class="green-40">Web Developer</a>
                 </div>
             </div>
         </div>
-    </main>
+    <svg width="1519" height="269" viewBox="0 0 1519 269" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 253.407L71.0417 259.44C143.083 265.474 287.167 277.541 431.25 259.44C575.333 241.34 719.417 193.072 863.5 187.038C1007.58 181.005 1151.67 217.206 1295.75 199.105C1439.83 181.005 1583.92 108.603 1655.96 72.4019L1728 36.201V0H1655.96C1583.92 0 1439.83 0 1295.75 0C1151.67 0 1007.58 0 863.5 0C719.417 0 575.333 0 431.25 0C287.167 0 143.083 0 71.0417 0H-1V253.407Z" fill="#123C35"/>
+    </svg>
+
+
+        <!-- promo specialist -->
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2>Our best specialist</h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-4"></div>
+                    <div class="col-4"></div>
+                    <div class="col-4"></div>
+                    <div class="col-4"></div>
+                    <div class="col-4"></div>
+                    <div class="col-4"></div>
+                </div>
+
+                <div class="col-12 d-flex justify-content-center">
+                    <button class="btn">Discover More</button>
+                </div>
+            </div>
+            <!-- specialist banner -->
+            <div class="row bg-green my-5">
+                <div class="col-5">
+                    <h1>
+                        Find the talent needed to get your business growing.
+                    </h1>
+                    <button class="btn">Get Started</button>
+                </div>
+            </div>
+            <!-- find next job section -->
+            <div class="row my-5">
+                <div class="col-5">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <div class="col-5 offset-2 d-flex flex-column">
+                    <div class="next-job">
+                        <h3>Title</h3>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Ab magni sapiente neque recusandae reiciendis,
+                            perspiciatis magnam ducimus maxime voluptatibus
+                            natus!
+                        </p>
+                    </div>
+                    <div class="next-job">
+                        <h3>Title</h3>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Ab magni sapiente neque recusandae reiciendis,
+                            perspiciatis magnam ducimus maxime voluptatibus
+                            natus!
+                        </p>
+                    </div>
+                    <div class="next-job">
+                        <h3>Title</h3>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Ab magni sapiente neque recusandae reiciendis,
+                            perspiciatis magnam ducimus maxime voluptatibus
+                            natus!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer -->
+        <div class="container my-5">
+            <div class="row">footer</div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -109,7 +155,27 @@ export default {
 <style lang="scss" scoped>
 @import "./../../sass/app";
 
-.bg-green{
+//UTILITIES
+.krona {
+    font-family: $ff-heading;
+}
+.raleway {
+    font-family: $ff-body;
+}
+.green-40{
+    color: $green-40;
+}
+.bg-green-55 {
+    background-color: $green-55;
+}
+.bg-green-60{
     background-color: $green-60;
 }
+.yellow-60 {
+    color: $yellow-60;
+}
+.bg-green-50{
+    background-color: $green-50;
+}
+//BODY
 </style>
