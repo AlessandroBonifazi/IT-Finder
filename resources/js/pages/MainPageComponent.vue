05153262bbee6ddc963116f38256c8d3c7d0b034
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-06-29 11:33:10 +0200

    landing page init

--- resources/js/pages/MainPageComponent.vue
+++ resources/js/pages/MainPageComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <!-- <div>
         <h1>Main</h1>
         <router-link to="/register">register</router-link>
         <button
@@ -11,12 +11,24 @@
         >
             Logout
         </button>
-    </div>
+    </div> -->
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <h1>Your Next IT Specialist is Here</h1>
+                    <p>Search and find best IT Specialist from all around the world for your one million project idea or business</p>
+                    <button class="btn">Discover More</button>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
 export default {
     name: "MainPage",
+
     data() {
         return {
             user: {},
@@ -32,4 +44,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "./../../sass/app";
+</style>
