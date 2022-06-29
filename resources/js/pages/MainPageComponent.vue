409ec4a64c356cdbf5b2afbc5d17b321cccc5a50
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-06-30 01:17:51 +0200

    upd main page

--- resources/js/pages/MainPageComponent.vue
+++ resources/js/pages/MainPageComponent.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="m-0 p-0">
+        <HeaderComponent />
         <!-- banner -->
         <div class="row my-5 justify-content-start mx-5 px-5">
             <div class="col-5 mx-5 px-5">
-                <h1 class="krona green-55 mb-4 mt-5">Your Next <span class="krona yellow-60"> IT </span>Specialist is Here</h1>
+                <h1 class="krona green-55 mb-4 mt-5">
+                    Your Next
+                    <span class="krona yellow-60"> IT </span>Specialist is Here
+                </h1>
                 <p>
                     Search and find best IT Specialist from all around the world
                     for your one million project idea or business
@@ -12,10 +16,31 @@
             </div>
         </div>
         <!-- specialist role -->
-        <svg width="1519" height="240" viewBox="0 0 1519 240" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" fill="#14453D"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" fill="#123C35"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z" stroke="black"/>
+        <svg
+            width="1519"
+            height="240"
+            viewBox="0 0 1519 240"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z"
+                fill="#14453D"
+            />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z"
+                fill="#123C35"
+            />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-1 182.559H95.5358C190.631 182.559 383.702 182.559 575.333 149.053C766.964 115.546 960.036 48.5316 1151.67 19.8114C1343.3 -8.90886 1536.37 0.664558 1631.46 5.45127L1728 10.238V240H1631.46C1536.37 240 1343.3 240 1151.67 240C960.036 240 766.964 240 575.333 240C383.702 240 190.631 240 95.5358 240H-1V182.559Z"
+                stroke="black"
+            />
         </svg>
 
         <div class="container-fluid bg-green-55">
@@ -29,36 +54,62 @@
                 </div>
             </div>
             <div class="row specialist flex-wrap justify-content-center">
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Front-End</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Back-End</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Mobile</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Analyst</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Web Developer</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Web Developer</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Web Developer</a>
                 </div>
-                <div class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center">
+                <div
+                    class="col-3 text-center my-2 krona bg-green-60 py-3 mx-1 align-self-center"
+                >
                     <a href="#" class="green-40">Web Developer</a>
                 </div>
             </div>
         </div>
-    <svg width="1519" height="269" viewBox="0 0 1519 269" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M-1 253.407L71.0417 259.44C143.083 265.474 287.167 277.541 431.25 259.44C575.333 241.34 719.417 193.072 863.5 187.038C1007.58 181.005 1151.67 217.206 1295.75 199.105C1439.83 181.005 1583.92 108.603 1655.96 72.4019L1728 36.201V0H1655.96C1583.92 0 1439.83 0 1295.75 0C1151.67 0 1007.58 0 863.5 0C719.417 0 575.333 0 431.25 0C287.167 0 143.083 0 71.0417 0H-1V253.407Z" fill="#123C35"/>
-    </svg>
-
+        <svg
+            width="1519"
+            height="269"
+            viewBox="0 0 1519 269"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M-1 253.407L71.0417 259.44C143.083 265.474 287.167 277.541 431.25 259.44C575.333 241.34 719.417 193.072 863.5 187.038C1007.58 181.005 1151.67 217.206 1295.75 199.105C1439.83 181.005 1583.92 108.603 1655.96 72.4019L1728 36.201V0H1655.96C1583.92 0 1439.83 0 1295.75 0C1151.67 0 1007.58 0 863.5 0C719.417 0 575.333 0 431.25 0C287.167 0 143.083 0 71.0417 0H-1V253.407Z"
+                fill="#123C35"
+            />
+        </svg>
 
         <!-- promo specialist -->
         <div class="container">
@@ -85,20 +136,19 @@
                 <div class="col-12 bg-green-55 d-flex specialist-banner">
                     <div class="row d-flex">
                         <div class="col-5 my-5 mx-3">
-
                             <h1 class="krona find-talent-title">
-                                Find the talent needed to get your business <span class="krona yellow-60">growing</span>.
+                                Find the talent needed to get your business
+                                <span class="krona yellow-60">growing</span>.
                             </h1>
-                             <button class="btn get-started px-5 mt-5">Get Started</button>
-
+                            <button class="btn get-started px-5 mt-5">
+                                Get Started
+                            </button>
                         </div>
 
                         <div class="col-7 talent-img">
-                            <img src="" alt="talent">
+                            <img src="" alt="talent" />
                         </div>
-                       
                     </div>
-                    
                 </div>
             </div>
             <!-- find next job section -->
@@ -143,13 +193,14 @@
         <div class="container my-5">
             <div class="row">footer</div>
         </div>
+        <a href="#" @click="logout">logout</a>
     </div>
 </template>
 
 <script>
+import HeaderComponent from "./../components/HeaderComponent.vue";
 export default {
     name: "MainPage",
-
     data() {
         return {
             user: {},
@@ -159,9 +210,11 @@ export default {
         logout() {
             window.axios.post("/logout").then(() => {
                 // this.$router.push("/");
+                console.log("logout");
             });
         },
     },
+    components: { HeaderComponent },
 };
 </script>
 
@@ -175,7 +228,7 @@ export default {
 .raleway {
     font-family: $ff-body;
 }
-.green-40{
+.green-40 {
     color: $green-40;
 }
 .green-55 {
@@ -184,50 +237,50 @@ export default {
 .bg-green-55 {
     background-color: $green-55;
 }
-.bg-green-60{
+.bg-green-60 {
     background-color: $green-60;
 }
 .yellow-60 {
     color: $yellow-60;
 }
-.bg-green-50{
+.bg-green-50 {
     background-color: $green-50;
 }
 
 //BODY
-.specialist a:hover{
+.specialist a:hover {
     color: white;
 }
-.discover{
+.discover {
     background-color: $btn-primary-bg;
     font-size: 20px;
     box-shadow: $box-shadow-secondary;
 }
-.discover:hover{
+.discover:hover {
     background-color: $btn-primary-bg-hoover;
 }
-.discover:active{
+.discover:active {
     background-color: $btn-primary-bg-active;
     color: white;
 }
-.get-started{
+.get-started {
     background-color: $btn-tertiary-bg;
     font-size: 20px;
-    
 }
-.discover, .get-started{
+.discover,
+.get-started {
     color: white;
     border: none;
     border-radius: 8px;
 }
-.find-talent{
+.find-talent {
     border-radius: 8px;
 }
-.find-talent-title{
+.find-talent-title {
     color: white;
     font-size: 28px;
 }
-.specialist-banner{
+.specialist-banner {
     border-radius: 9px;
 }
 </style>
