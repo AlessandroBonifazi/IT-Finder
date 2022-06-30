9775672b89c7553abde49dcb8022f5c7f6adbc6b
Robin9800 <96703277+Robin9800@users.noreply.github.com>
2022-06-30 19:14:04 +0200

    DevDetailComponent init

--- resources/js/components/DevDetailComponent.vue
+++ resources/js/components/DevDetailComponent.vue
@@ -1,9 +1,46 @@
 <template>
-    <div class="row jumbotron m-0">
-        <div class="col-12 d-flex">
-            <img class="rounded-circle dev-img" src="img\unsplash_iEEBWgY_6lA.png" alt="">
+<div>
+    <div class="row jumbotron m-0"></div>
+    <div class=" col-10 container">
+
+        <!--IMG AND PROFILE-->
+        <div class="d-flex flex-column">
+            <div class="d-flex dev-img-rating position-absolute">
+                <img class="rounded-circle dev-img " src="img\unsplash_iEEBWgY_6lA.png" alt="">
+                <p class="rating px-2 rounded-pill yellow-60 m-0">4.5</p>
+
+            </div>
+
+            <div class="info align-self-center mt-5 mx-3">
+                <h1 class="krona dark-grey">Lucio Melis</h1>
+                <p class="raleway coral-50">Full Stack Web Developer
+                    <span class="mx-2">|</span>
+                    Web Designer
+                </p>
+            </div>
         </div>
+
+        <!--ABOUT ME-->
+        <div class="about-me col-8">
+            <h5 class="krona green-55">About Me</h5>
+            <p>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi. 
+
+            </p>
+        </div>
+        
+        <!--TECHNOLOGIES STACK-->
+        <div class="technlogies-stack col-5">
+            <h5 class="krona green-55">My Technologies Stack</h5>
+        </div>
+
+        <!--PEOPLE'S REVIEWS-->
+        <div class="revies col-7">
+            <h5 class="krona green-55">People’s Reviews</h5>
+        </div>
+        
     </div>
+</div>
+    
 </template>
 
 <script>
@@ -41,17 +78,35 @@ export default {
 .bg-green-50{
     background-color: $green-50;
 }
-
+.clear-grey{
+    color: $fc-grey-clear;
+}
+.dark-grey{
+    color: $fc-grey-dark;
+}
 //BODY
+.dev-detail-container{
+    margin-right: 0px;
+}
 .jumbotron{
     width: 100%;
     background-image: url("/img/Wonder.png");
+    border-radius: 0%;
+}
+.dev-img-rating{
+    top: -65px;
 }
 .dev-img{
-    width: 12%;
+    width: 200px;
     object-fit: cover;
-    position: absolute;
-    top: -14px;
-    left: 119px;
 }
+.rating{
+    position: absolute;
+    color: black;
+    background-color: $yellow-50;
+    font-size: 18px;
+    left: 152px;
+    top: 20px;
+}
+
 </style>
