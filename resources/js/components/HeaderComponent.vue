6a25d9976a32044fcaf215a0fcd6ee645b862a48
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-05 01:38:38 +0200

    solved a main menu bug

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -189,7 +189,7 @@ export default {
     width: 100%;
     height: 100%;
     background-color: $bg-secondary;
-    z-index: 1;
+    z-index: 10;
     display: flex;
     flex-direction: column;
     justify-content: center;
