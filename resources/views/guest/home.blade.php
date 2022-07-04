d8f6e65a179f3f0002fa956caccf937e3953711b
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-05 00:49:57 +0200

    page title done

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ITFinder') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/front-app.js') }}" defer></script>
