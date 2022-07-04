bcf1917219e01118c05ee9b471e72faf39b4864a
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-07-05 00:26:37 +0200

    update table technology

--- database/migrations/2022_06_24_084847_create_technologies_table.php
+++ database/migrations/2022_06_24_084847_create_technologies_table.php
@@ -16,7 +16,7 @@ class CreateTechnologiesTable extends Migration
         Schema::create("technologies", function (Blueprint $table) {
             $table->id();
             $table->string("name", 20);
-            $table->string("logo");
+            $table->string("logo")->nullable();;
             $table->timestamps();
         });
     }
