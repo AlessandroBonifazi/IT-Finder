3396387a92e0d2097feeaaff214299e2c8acfdd4
gioiascari <gioia.ascari96@gmail.com>
2022-07-05 14:33:05 +0200

    update checkout file and created confirmed page blade file

--- resources/views/auth/checkout.blade.php
+++ resources/views/auth/checkout.blade.php
@@ -2,28 +2,76 @@
 @section('content')
 
     <div class="container">
-@dd($promo)
+        <div class="col-sm-10 col-md-10 col-lg-9 offset-lg-1">
+            <div class="row">
+                <div class="order-sm-2 order-md-1 col-sm-10 col-md-6 col-lg-8">
+                    <div class="row">
+                        {{-- @dd($promo) --}}
+
+                        {{-- Promo --}}
+                        <div style="height: 500px; "
+                            class="col-md-12 col-lg-6 grid-margin stretch-card my-3 overflow-auto h-100">
+                            <div class="card p-3">
+                                <div class="card-body  overflow-auto">
+                                    {{-- title --}}
+                                    <h5 class="card-title">You choose Type: {{$promo->type}}</h5>
+                                            <li class="list-group-item w-75">
+                                                <p>Duration: {{ $promo->duration }} week</p>
+                                                Price: {{ $promo->price }}$</li>
+                                </div>
+                                <div>
+                                    Are you sure?
+                                    <p>Continue here for the payment or <div><a href="{{route ('user.checkin')}}">Go Back</a></div></p>
+                                    <form id="payment-form" action="{{ url('/user/confirmed')}}" method="PUT" enctype="multipart/form-data">
+                                        @csrf
+                                        {{-- @method('PUT') --}}
+                                        <section>
+                                            <label for="amount">
+                                                <div class="input-wrapper amount-wrapper">
+                                                    <input type="hidden" id="amount" name="amount" min="1" placeholder="amount"
+                                                        value="{{ $promo->price }}" readonly>
+                                                </div>
+                                            </label>
+
+                                            <input type="hidden" name="id" value="{{ $id }}">
 
 
+                                            <input type="hidden" name="token" value="{{ $token }}">
 
+
+                                            <div class="bt-drop-in-wrapper">
+                                                <div id="bt-dropin"></div>
+                                            </div>
+                                            <input type="hidden" name="payment_method_nonce" value="fake-valid-visa-nonce" id="nonce">
+                                        </section>
+                                        <div class="d-flex w-100 justify-content-between align-item-baseline">
+                                            <button class="button btn btn-success" type="submit"><span>BUY</span></button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 
 
-
-    <!-- includes the Braintree JS client SDK -->
-    {{-- <script src="https://js.braintreegateway.com/web/dropin/1.31.0/js/dropin.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/dropin/1.31.0/js/dropin.min.js"></script>
 
     <script>
         var form = document.querySelector('#payment-form');
         var client_token = "{{ $token }}";
-        // var nonc =  document.querySelector('#nonce');
         braintree.dropin.create({
             authorization: client_token,
             selector: '#bt-dropin',
         }, function(createErr, instance) {
             if (createErr) {
                 console.log('Create Error', createErr);
-                // return;
             }
             form.addEventListener('submit', function(event) {
                 event.preventDefault();
@@ -35,12 +83,11 @@
                     //Add the nonce to the form and submit
                     document.querySelector('input[name="payment_method_nonce"]').value = payload
                         .nonce;
-                    //  nonc.value = "66";
                     form.submit();
                 });
             });
         });
 
-    </script> --}}
+</script>
 
 @endsection
