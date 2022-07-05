46b5f2dce9444eb211d1349f2520cb50e25acd31
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-07-06 00:56:42 +0200

    layout fix

--- resources/views/auth/checkout.blade.php
+++ resources/views/auth/checkout.blade.php
@@ -1,61 +1,56 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="container">
         <div class="col-sm-10 col-md-10 col-lg-9 offset-lg-1">
             <div class="row">
-                <div class="order-sm-2 order-md-1 col-sm-10 col-md-6 col-lg-8">
-                    <div class="row">
-                        {{-- @dd($promo) --}}
-
-                        {{-- Promo --}}
-                        <div style="height: 500px; "
-                            class="col-md-12 col-lg-6 grid-margin stretch-card my-3 overflow-auto h-100">
-                            <div class="card p-3">
-                                <div class="card-body  overflow-auto">
-                                    {{-- title --}}
-                                    <h5 class="card-title">You choose Type: {{$promo->type}}</h5>
-                                            <li class="list-group-item w-75">
-                                                <p>Duration: {{ $promo->duration }} week</p>
-                                                Price: {{ $promo->price }}$</li>
-                                </div>
-                                <div>
-                                    Are you sure?
-                                    <p>Continue here for the payment or <div><a href="{{route ('user.checkin')}}">Go Back</a></div></p>
-                                    <form id="payment-form" action="{{ route('user.payment')}}" method="POST" enctype="multipart/form-data">
-                                        @csrf
-                                        @method('PUT')
-                                        <section>
-                                            <label for="amount">
-                                                <div class="input-wrapper amount-wrapper">
-                                                    <input type="hidden" id="amount" name="amount" min="1" placeholder="amount"
-                                                        value="{{ $promo->price }}" readonly>
-                                                </div>
-                                            </label>
-
-                                            <input type="hidden" name="id" value="{{ $id }}">
-
-
-                                            <input type="hidden" name="token" value="{{ $token }}">
-
-
-                                            <div class="bt-drop-in-wrapper">
-                                                <div id="bt-dropin"></div>
-                                            </div>
-                                            <input type="hidden" name="payment_method_nonce" value="fake-valid-visa-nonce" id="nonce">
-                                        </section>
-                                        <div class="d-flex w-100 justify-content-between align-item-baseline">
-                                            <button class="button btn btn-success" type="submit"><span>BUY</span></button>
-                                        </div>
-                                    </form>
-
-                                </div>
-                            </div>
+                {{-- Promo --}}
+                <div style="height: 500px; " class="col-md-12 col-lg-6 grid-margin stretch-card my-3 overflow-auto h-100">
+                    <div class="card p-3">
+                        <div class="card-body  overflow-auto">
+                            {{-- title --}}
+                            <h5 class="card-title">You choose Type: {{ $promo->type }}</h5>
+                            <li class="list-group-item w-75">
+                                <p>Duration: {{ $promo->duration }} days</p>
+                                Price: {{ $promo->price }}$
+                            </li>
                         </div>
+                        <div>
+                            Are you sure?
+                            <p>Continue here for the payment or
+                            <div><a href="{{ route('user.checkin') }}">Go Back</a></div>
+                            </p>
+                            <form id="payment-form" action="{{ route('user.payment') }}" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+                                <section>
+                                    <label for="amount">
+                                        <div class="input-wrapper amount-wrapper">
+                                            <input type="hidden" id="amount" name="amount" min="1"
+                                                placeholder="amount" value="{{ $promo->price }}" readonly>
+                                        </div>
+                                    </label>
 
+                                    <input type="hidden" name="id" value="{{ $id }}">
+
+
+                                    <input type="hidden" name="token" value="{{ $token }}">
+
+
+                                    <div class="bt-drop-in-wrapper">
+                                        <div id="bt-dropin"></div>
+                                    </div>
+                                    <input type="hidden" name="payment_method_nonce" value="fake-valid-visa-nonce"
+                                        id="nonce">
+                                </section>
+                                <div class="d-flex w-100 justify-content-between align-item-baseline">
+                                    <button class="button btn btn-success" type="submit"><span>BUY</span></button>
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -87,7 +82,5 @@
                 });
             });
         });
-
-</script>
-
+    </script>
 @endsection
