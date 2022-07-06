f75a876f091b79518e30bbc09775bf51eadb40f9
erik schievenin <erik.schievenin@gmail.com>
2022-07-06 16:23:54 +0200

    Create graphics standard with CHARTJS, but empty filter data

--- app/Http/Controllers/User/StatisticController.php
+++ app/Http/Controllers/User/StatisticController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\User;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\Review;
 
 use App\User;
 use App\Contact;
@@ -18,13 +20,22 @@ class StatisticController extends Controller
         $messages = $user->messages->all();
         $reviews = $user->reviews->all();
 
-        // dd(count($messages));
+        $countMessages = [];
+        foreach($messages as $mess){
+            $countMessages[]=$mess['messages'];
+        };
+
+        // $valutation = Review::user();
+        // $voto = $review -> all();
+
+        // dd($valutation);
+        // dd(count($reviews));
 
         $chartjs = app()->chartjs
         ->name('lineChartTest')
         ->type('line')
         ->size(['width' => 500, 'height' => 250])
-        ->labels(['January','Febraury','January','January','January','January','January','January','January','January','January','January'])
+        ->labels(['January','Febraury','March','April','May','June','July','August','September','October','November','December'])
         ->datasets([
             [
                 "label" => "Messages",
@@ -34,7 +45,7 @@ class StatisticController extends Controller
                 "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
                 "pointHoverBackgroundColor" => "#fff",
                 "pointHoverBorderColor" => "rgba(220,220,220,1)",
-                'data' => [count($messages)],
+                'data' => [count($countMessages)],
             ],
             [
                 "label" => "Reviews",
@@ -44,11 +55,31 @@ class StatisticController extends Controller
                 "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
                 "pointHoverBackgroundColor" => "#fff",
                 "pointHoverBorderColor" => "rgba(220,220,220,1)",
-                'data' => [12, 33, 44, 44, 55, 23, 40],
+                'data' => [count($reviews)],
             ]
         ])
         ->options([]);
 
-return view('auth.statistics', compact('chartjs','user'));
+        $chart2js = app()->chartjs
+        ->name('barChartTest')
+        ->type('bar')
+        ->size(['width' => 400, 'height' => 200])
+        ->labels(['Label x', 'Label y'])
+        ->datasets([
+            [
+                "label" => "Valutation",
+                'backgroundColor' => ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+                'data' => [15,85,96,35,65]
+            ],
+            [
+                "label" => "My First dataset",
+                'backgroundColor' => ['rgba(255, 99, 132, 0.3)', 'rgba(54, 162, 235, 0.3)'],
+                'data' => [65, 12]
+            ]
+        ])
+        ->options([]);
+
+
+return view('auth.statistics', compact('chartjs','chart2js','user'));
 }
 }
--- resources/views/auth/statistics.blade.php
+++ resources/views/auth/statistics.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>Prova e inserimento grafico</h2>
+    <h2 style="text-align:center; margin-top: 15px;">Statistics Messages/Reviews</h2>
 
-    <div style="width:75%;">
+    <div style="width:50%; margin:auto; margin-top: 15px;">
         {!! $chartjs->render() !!}
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    {{-- GRAFICO STANDARD OF CHARTJS - DON'T WORK --}}
+    <h2 style="text-align:center; margin-top: 15px;">Statistics Valutation</h2>
 
-    {{-- <canvas id="myChart" width="600" height="400"></canvas> --}}
+    <div style="width:50%; margin:auto; margin-top: 15px;">
+        {!! $chart2js->render() !!}
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 @endsection
