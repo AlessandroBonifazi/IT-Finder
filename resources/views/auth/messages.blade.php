caf50f4c1e567b7cd8903725ae600c31e72c51b1
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-07-07 03:48:16 +0200

    added card for messages and reviews

--- resources/views/auth/messages.blade.php
+++ resources/views/auth/messages.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- Messages --}}
     <div class="order-sm-2 order-md-1 col-sm-10 col-md-6 col-lg-8">
         <div class="row">
-
-            {{-- Messaggi --}}
-            <div class="col-md-12 col-lg-6 grid-margin stretch-card my-3">
-                <div class="card">
-                    <div class="card-body py-5">
-                        {{-- title --}}
-                        <h5 class="card-title">Messages</h5>
-
-                        @foreach ($messages as $message)
-                            <ul class="list-group list-group-horizontal my-1">
-                                <li class="list-group-item">
-                                    <h5>{{ $message->user_name }}</h5>
-                                    <span>{{ $message->created_at }}</span>
-                                </li>
-                                <li class="list-group-item w-75">{{ $message->content }}</li>
-                            </ul>
-                        @endforeach
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="itf-card">
+                            {{-- title --}}
+                            <div class="itf-card-header">
+                                <h5 class="itf-card-title">Messages</h5>
+                            </div>
+                            <div class="itf-card-body py-5">
+                                @foreach ($messages as $message)
+                                    <ul class="list-group list-group-horizontal my-1">
+                                        <li class="list-group-item">
+                                            <h5>{{ $message->user_name }}</h5>
+                                            <span>{{ $message->created_at }}</span>
+                                        </li>
+                                        <li class="list-group-item w-75">{{ $message->content }}</li>
+                                    </ul>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/auth/reviews.blade.php
+++ resources/views/auth/reviews.blade.php
@@ -1,26 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- Reviews --}}
     <div class="order-sm-2 order-md-1 col-sm-10 col-md-6 col-lg-8">
         <div class="row">
-
-            {{-- Messaggi --}}
-            <div class="col-md-12 col-lg-6 grid-margin stretch-card my-3">
-                <div class="card">
-                    <div class="card-body py-5">
-                        {{-- title --}}
-                        <h5 class="card-title">Reviews</h5>
-
-                        @foreach ($reviews as $review)
-                            <ul class="list-group list-group-horizontal my-1">
-                                <li class="list-group-item">
-                                    <h5>{{ $review->user_name }}</h5>
-                                    <span>{{ $review->valutation }}</span>
-                                    <span>{{ $review->created_at }}</span>
-                                </li>
-                                <li class="list-group-item w-75">{{ $review->content }}</li>
-                            </ul>
-                        @endforeach
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="itf-card">
+                            {{-- title --}}
+                            <div class="itf-card-header">
+                                <h5 class="itf-card-title">Reviews</h5>
+                            </div>
+                            <div class="itf-card-body py-5">
+                                @foreach ($reviews as $review)
+                                    <ul class="list-group list-group-horizontal my-1">
+                                        <li class="list-group-item">
+                                            <h5>{{ $review->user_name }}</h5>
+                                            <span>{{ $review->valutation }}</span>
+                                            <span>{{ $review->created_at }}</span>
+                                        </li>
+                                        <li class="list-group-item w-75">{{ $review->content }}</li>
+                                    </ul>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
