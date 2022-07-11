efc459474c01c92a832c3e9eb89b544b642241e1
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-11 16:40:26 +0200

    no messages control

--- resources/views/auth/messages.blade.php
+++ resources/views/auth/messages.blade.php
@@ -6,28 +6,46 @@
         <div class="itf-card">
             {{-- title --}}
             <div class="itf-card-header">
-                <h5 class="itf-card-title">Messages</h5>
+                <h5 class="itf-card-title">{{ $messages->total_messages }} received messages</h5>
             </div>
+            @if (!$is_premium)
+                <div class="itf-card-info-bar">
+                    <a href="{{ route('user.checkin') }}" class="itf-card-title itf-card-title-small">Upgrade
+                        your profile to receive
+                        more messages</a>
+                </div>
+            @endif
+            {{-- body --}}
             <div class="itf-card-body">
-                @foreach ($messages as $message)
-                    <div class="itf-message-item">
-                        <div class="itf-message-item-header">
-                            @if ($message->user_name)
-                                <h5 class="itf-message-name">{{ $message->user_name }}</h5>
-                            @else
-                                <h5 class="itf-message-name">Anonymous</h5>
-                            @endif
-                            @if ($message->created_at)
-                                <span class="itf-message-date">{{ $message->created_at->diffForHumans() }}</span>
-                            @endif
+                @if ($messages->count() > 0)
+                    @foreach ($messages as $message)
+                        <div class="itf-message-item">
+                            <div class="itf-message-item-header">
+                                @if ($message->user_name)
+                                    <h5 class="itf-message-name">{{ $message->user_name }}</h5>
+                                @else
+                                    <h5 class="itf-message-name">Anonymous</h5>
+                                @endif
+                                @if ($message->created_at)
+                                    <span class="itf-message-date">{{ $message->created_at->diffForHumans() }}</span>
+                                @endif
+                            </div>
+                            <div class="itf-message-item-body">
+                                <p class="itf-message-content">
+                                    {{ $message->content }}
+                                </p>
+                            </div>
                         </div>
+                    @endforeach
+                @else
+                    <div class="itf-message-item">
                         <div class="itf-message-item-body">
                             <p class="itf-message-content">
-                                {{ $message->content }}
+                                No messages yet.
                             </p>
                         </div>
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
 
