869d61b33cedfab1bc1034ed4fc6e88f5ab645d6
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-12 01:49:27 +0200

    team data file created

---
+++ config/teamDatas.php
@@ -0,0 +1,153 @@
+<?php
+
+return[
+    [
+        'name'=>'Gioia',
+        'surname'=>'Ascari',
+        'user_name'=>'gioiascari',
+        'img_path'=>'https://media-exp2.licdn.com/dms/image/C4D03AQEuzZrF-mIoqA/profile-displayphoto-shrink_800_800/0/1571049815915?e=1663200000&v=beta&t=c2Is8fG4kD3L_lTl3T0XpzCnq6HKWYRJ_ScMvHT0lsI',
+        'email'=>'gioia.ascari96@gmail.com',
+        'password'=>'12345678',
+        'location'=>'Venezia',
+        'job_experience'=> 1,
+        'cv'=>'Ciao sono Gioia del team 3. Sono aperta a qualsaisi posizione lavorativa.
+        Sono una persona solare ma nei momenti difficili sono intrattabile. Mi piace imparare ogni giorno cose nuove.',
+        'contact_email'=>'gioia.ascari96@gmail.com',
+        'linkedin'=>'https://www.linkedin.com/in/gioia-ascari-978199196/',
+        'phone'=>'1234566789',
+        'github'=>'https://github.com/gioiascari',
+        'site'=>'/',
+        'techs'=>[
+            23,32,58,57,72,76,84,95,114,139,166,165,74,4,113
+        ],
+        'specializations'=>[
+            3,13
+        ]
+
+    ],
+
+    [
+            'name'=>'Alessandro',
+            'surname'=>'Bonifazi',
+            'user_name'=>'ale',
+            'img_path'=>'https://media-exp2.licdn.com/dms/image/C4E03AQHIREhCt-0PVQ/profile-displayphoto-shrink_800_800/0/1656463994837?e=1663200000&v=beta&t=HJIN6Dh3Gtprc6ohqdDzEPBRFi5yS52BCr5GMxKGAnc',
+            'email'=>'alessandrobonifazi21@gmail.com',
+            'password'=>'12345678',
+            'location'=>'Roma',
+            'job_experience'=> 1,
+            'cv'=>'Ciao sono Alessandro del team 3. Sono aperto a qualsaisi posizione lavorativa. Mi piace imparare ogni giorno cose nuove.',
+            'contact_email'=>'alessandrobonifazi21@gmail.com',
+            'linkedin'=>'https://www.linkedin.com/in/alessandro-bonifazi-0693b6217/',
+            'phone'=>'1234566789',
+            'github'=>'https://github.com/AlessandroBonifazi',
+            'site'=>'/',
+            'techs'=>[
+                23,32,58,57,72,76,84,95,114,139,166,165
+            ],
+            'specializations'=>[
+                3
+            ]
+
+        ],
+
+    [
+                'name'=>'Mykhaylo',
+                'surname'=>'Tymofyeyev ',
+                'user_name'=>'Mykhaylo',
+                'img_path'=>'https://media-exp2.licdn.com/dms/image/C4E03AQFch0TTXJFITA/profile-displayphoto-shrink_800_800/0/1655739170035?e=1663200000&v=beta&t=gXVo2djbA5Rlq4oLiD5ruZFdoBfHFtY_00DxTCCCLjc',
+                'email'=>'mykhaylo.tymofyeyev@gmail.com',
+                'password'=>'12345678',
+                'location'=>'Milano',
+                'job_experience'=> 1,
+                'cv'=>'Sono fortemente interessato dello sviluppo web, principalmente lato client. Per costruire conoscenze di base solide ho preso parte ad un corso di FullStack Web Developer proposto da Boolean Careers Italia.',
+                'contact_email'=>'mykhaylo.tymofyeyev@gmail.com',
+                'linkedin'=>'https://www.linkedin.com/in/tymofyeyev/',
+                'phone'=>'1234566789',
+                'github'=>'https://github.com/sargon17',
+                'site'=>'https://sargon17.github.io/my-portfolio/',
+                'techs'=>[
+                    23,32,58,57,72,76,84,95,114,139,166,165
+                ],
+                'specializations'=>[
+                    3
+                ]
+
+    ],
+
+    [
+            'name'=>'Erik',
+            'surname'=>'Schievenin',
+            'user_name'=>'Eriksioxx',
+            'img_path'=>'https://media-exp2.licdn.com/dms/image/C4E03AQFjzoWfGcmcHw/profile-displayphoto-shrink_200_200/0/1657558728787?e=1663200000&v=beta&t=H5UhFfydXqDORHA9DhAjqGDJiljC3-y2kEPNYQVKJKU',
+            'email'=>'erik.schievenin@gmail.com',
+            'password'=>'12345678',
+            'location'=>'Treviso',
+            'job_experience'=>1,
+            'cv'=>'Ciao ragazzi, sono Erik del team 3, Sono un ragazzo intraprendente che si è cimentato in diversi campi lavorativi, realizzando negli anni importanti progetti in ambito artistico e multiculturale.',
+            'contact_email'=>'erik.schievenin@gmail.com',
+            'linkedin'=>'https://www.linkedin.com/in/erik-schievenin-8971256b',
+            'phone'=>'3469607521',
+            'github'=>'https://github.com/Eriksioxx',
+            'site'=>'/',
+            'techs'=>[
+                23,32,58,57,72,76,84,95,114,139,166,165
+            ],
+            'specializations'=>[
+                3,13
+            ]
+
+    ],
+
+    [
+        'name'=>'Robin',
+        'surname'=>'Costanzo',
+        'user_name'=>'Rob',
+        'img_path'=>'https://media-exp2.licdn.com/dms/image/C4E03AQHLPbL7ghElsA/profile-displayphoto-shrink_800_800/0/1643759733442?e=1663200000&v=beta&t=QnDJp5bWKW2_OevMmjXTFxsdo3ZVyuEXHwZD0dBqDyo',
+        'email'=>'robin.costanzo@gmail.com',
+        'password'=>'12345678',
+        'location'=>'Napoli',
+        'job_experience'=> 6,
+        'cv'=>'Ciao ragazzi, sono Robin del team 3, Sono un ragazzo intraprendente che si è cimentato in diversi campi lavorativi.',
+        'contact_email'=>'robin.costanzo@gmail.com',
+        'linkedin'=>'https://www.linkedin.com/in/robin-costanzo-0502a11b9/',
+        'phone'=>'3469607521',
+        'github'=>'https://github.com/Robin9800',
+        'site'=>'/',
+        'techs'=>[
+            23,32,58,57,72,76,84,95,114,139,166,165
+        ],
+        'specializations'=>[
+            3
+        ]
+
+    ],
+
+    [
+        'name'=>'Antonio',
+        'surname'=>'Iorio',
+        'user_name'=>'Dev Antoine',
+        'img_path'=>'https://media-exp2.licdn.com/dms/image/C4E03AQEWjb7VtNX91w/profile-displayphoto-shrink_800_800/0/1655661588133?e=1663200000&v=beta&t=rB3XO1T3JLPheAqUinwMySWxMvCGzXweTRDfH5xx73w',
+        'email'=>'antonio.iorio@gmail.com',
+        'password'=>'12345678',
+        'location'=>'Cosenza',
+        'job_experience'=> 1,
+        'cv'=>'Ciao ragazzi, son Antoine del team 3, Son un ragazzo intraprendente che si è cimentato in diversi campi lavorativi.',
+        'contact_email'=>'antonio.iorio@gmail.com',
+        'linkedin'=>'https://www.linkedin.com/in/antonio-iorio-8954bb242/',
+        'phone'=>'3469607521',
+        'github'=>'https://github.com/antonioiorio94',
+        'site'=>'/',
+        'techs'=>[
+            23,32,58,57,72,76,84,95,114,139,166,165
+        ],
+        'specializations'=>[
+            3
+        ]
+
+    ]
+
+
+]
+
+
+?>
