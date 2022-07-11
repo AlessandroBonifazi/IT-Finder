e78c66ce26d240caca6bb235d073aa9e35f44dcf
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-07-11 14:01:12 +0200

    updated messages page layout

--- resources/views/auth/messages.blade.php
+++ resources/views/auth/messages.blade.php
@@ -3,26 +3,33 @@
 @section('content')
     {{-- Messages --}}
     <div class="col-12">
-        <div class="row">
-            <div class="col-12">
-                <div class="itf-card">
-                    {{-- title --}}
-                    <div class="itf-card-header">
-                        <h5 class="itf-card-title">Messages</h5>
+        <div class="itf-card">
+            {{-- title --}}
+            <div class="itf-card-header">
+                <h5 class="itf-card-title">Messages</h5>
+            </div>
+            <div class="itf-card-body">
+                @foreach ($messages as $message)
+                    <div class="itf-message-item">
+                        <div class="itf-message-item-header">
+                            @if ($message->user_name)
+                                <h5 class="itf-message-name">{{ $message->user_name }}</h5>
+                            @else
+                                <h5 class="itf-message-name">Anonymous</h5>
+                            @endif
+                            @if ($message->created_at)
+                                <span class="itf-message-date">{{ $message->created_at->diffForHumans() }}</span>
+                            @endif
+                        </div>
+                        <div class="itf-message-item-body">
+                            <p class="itf-message-content">
+                                {{ $message->content }}
+                            </p>
+                        </div>
                     </div>
-                    <div class="itf-card-body py-5">
-                        @foreach ($messages as $message)
-                            <ul class="list-group list-group-horizontal my-1">
-                                <li class="list-group-item">
-                                    <h5>{{ $message->user_name }}</h5>
-                                    <span>{{ $message->created_at }}</span>
-                                </li>
-                                <li class="list-group-item w-75">{{ $message->content }}</li>
-                            </ul>
-                        @endforeach
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
+
     </div>
 @endsection
